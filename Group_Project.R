78a1003a453794ae85e69ffc3cd15472883b9fc8
kylesock <54775123+kylesock@users.noreply.github.com>
2023-02-08 21:20:40 +0000

    Update Group_Project.R
    Added commentary

--- Group_Project.R
+++ Group_Project.R
@@ -1,10 +1,12 @@
 library(tidyverse)
 library(countrycode) # continent group data
-library(GGally)
-fat <- read_csv('Fat_Supply_Quantity_Data.csv')%>% select(-`Unit (all except Population)`)%>% drop_na()
-protein <- read_csv('Protein_Supply_Quantity_Data.csv') %>% select(-`Unit (all except Population)`)%>% drop_na()
-kcal <- read_csv('Food_Supply_kcal_Data.csv')%>% select(-`Unit (all except Population)`) %>% drop_na()
-kg <- read_csv('Food_Supply_Quantity_kg_Data.csv') %>% select(-`Unit (all except Population)`)%>% drop_na()
+library(corrplot)
+
+#read files in - focus is on kg dataset
+fat <- read_csv('data/Fat_Supply_Quantity_Data.csv')%>% select(-`Unit (all except Population)`)%>% drop_na()
+protein <- read_csv('data/Protein_Supply_Quantity_Data.csv') %>% select(-`Unit (all except Population)`)%>% drop_na()
+kcal <- read_csv('data/Food_Supply_kcal_Data.csv')%>% select(-`Unit (all except Population)`) %>% drop_na()
+kg <- read_csv('data/Food_Supply_Quantity_kg_Data.csv') %>% select(-`Unit (all except Population)`)%>% drop_na()
 
 # Convert <2.5 in undernourished column to 0, and set as numeric column
 kg <- kg %>% 
@@ -17,19 +19,21 @@ kg <- kg %>%
          )
 # correlation matrix without Country label column
 kg_cor <- kg %>% select(-Country) %>% cor(.)
+
+# Correlation plot illustrating as lot of correltions
+corrplot(kg_cor, type='upper')
+
+# vector of column names
 column_names <- colnames(kg)
 
+# add continent column, and factor
 kg_cont <- kg %>% 
   mutate(Continent = factor(countrycode(Country, 'country.name', 'continent'))) %>%
   select(c('Country', 'Continent', column_names[2:length(column_names)])) %>%
-  pivot_longer(cols = colnames(kg_cont)[3:length(kg_cont)],
+  pivot_longer(cols = colnames(.)[3:length(.)],
                names_to='variable',
                values_to='value')
 
-kg_cont_long <- kg_cont %>%
-  pivot_longer(cols = colnames(kg_cont)[3:length(kg_cont)],
-               names_to='variable',
-               values_to='value')
-
-plots <- ggplot(kg_cont_long, aes(x=value, colour=Continent, fill=Continent)) 
+# plot densities if each variable to evaluate efficacy of clustering methods
+plots <- ggplot(kg_cont, aes(x=value, colour=Continent, fill=Continent)) 
 plots + geom_density(alpha=0.5) + facet_wrap(~ variable, scales='free')
